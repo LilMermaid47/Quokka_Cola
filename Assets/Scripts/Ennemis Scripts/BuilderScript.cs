e6db66e841333ace9ae3fe9265bdb69617b465e7
Ariel <34323415+Gopir1@users.noreply.github.com>
2019-02-20 11:42:24 -0500

    Builder v1
    He flee
    He build

--- Assets/Scripts/Ennemis Scripts/BuilderScript.cs
+++ Assets/Scripts/Ennemis Scripts/BuilderScript.cs
@@ -1,46 +1,68 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class BuilderScript : MonoBehaviour
 {
+
     [SerializeField]
     float buildRayon = 10f;
     [SerializeField]
-    float buildFrequence = 100f;
+    int buildFrequence = 1000;
     [SerializeField]
-    Object usine;
+    float playerDistance = 15f;
+    [SerializeField]
+    Transform playerTransform;
+
 
     private NavMeshAgent navMeshAgent;
     private bool isNotBuilding = true;
-    private bool estSurPosition = true;
     private int randomBuildNumber;
     private Vector3 positionBuild;
     private float randomBuildX;
     private float randomBuildZ;
+    private Vector3 fleeVector;
+    private float fleeDistance;
+
     // Start is called before the first frame update
     void Start()
     {
-        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
-    
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-        if (isNotBuilding && randomBuildNumber == 0)
+        float distance = Vector3.Distance(transform.position, playerTransform.position);
+        if (distance < playerDistance)
         {
-            randomBuildNumber = Random.Range(0, 101);
-            isNotBuilding = false;
-            randomBuildX = Random.Range(0.0f, buildRayon);
-            randomBuildZ = Random.Range(0.0f, buildRayon);
-            positionBuild = new Vector3(transform.position.x + randomBuildX, 2, transform.position.z + randomBuildZ);
-            Debug.Log(positionBuild);
-            navMeshAgent.SetDestination(positionBuild);
-
+            RunAway();
         }
-        if (this.transform.position.x == randomBuildX)
-        { object usineInstance = Instantiate(usine, this.transform); }
+        else
+        {
+            randomBuildNumber = UnityEngine.Random.Range(0, (int)buildFrequence + 1);
+            if (randomBuildNumber == 1)
+            {
+                BuildNewBuilding();
+            }
+        }
+    }
+
+    private void RunAway()
+    {
+        fleeVector = transform.position + (transform.position - playerTransform.position);
+        navMeshAgent.SetDestination(fleeVector);
+    }
+
+    private void BuildNewBuilding()
+    {
+        isNotBuilding = false;
+        randomBuildX = UnityEngine.Random.Range(0.0f, buildRayon);
+        randomBuildZ = UnityEngine.Random.Range(0.0f, buildRayon);
+        positionBuild = new Vector3(transform.position.x + randomBuildX, 0, transform.position.z + randomBuildZ);
+        Debug.Log(positionBuild);
+        navMeshAgent.SetDestination(positionBuild);
     }
 }
