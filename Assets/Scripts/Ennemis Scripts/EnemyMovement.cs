18242883ad1dfa3eb83ff36bfb46090c34644029
Ariel <34323415+Gopir1@users.noreply.github.com>
2019-02-25 15:04:23 -0500

    usine + new movement + builder

--- Assets/Scripts/Ennemis Scripts/EnemyMovement.cs
+++ Assets/Scripts/Ennemis Scripts/EnemyMovement.cs
@@ -6,13 +6,13 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField]
-    Transform endPosition;
-
     NavMeshAgent navMeshAgent;
+    GameObject player;
+    Vector3 position;
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
@@ -23,7 +23,7 @@ public class EnemyMovement : MonoBehaviour
 
     private void SetDestination()
     {
-        Vector3 targetVector = endPosition.transform.position;
-        navMeshAgent.SetDestination(targetVector);
+        position = player.transform.position;
+        navMeshAgent.SetDestination(position);
     }
 }
